73f5f39f5364ce2228c5ab3192efaa67a22b9944
housedone <kimws930@gmail.com>
2021-10-30 11:45:51 +0900

    SwiftUI 튜토리얼 - SwiftUI 에센셜 - View 만들기와 합치기

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -7,13 +7,47 @@
 
 import SwiftUI
 
+// View 프로토콜을 준수하고 View의 내용과 레이아웃을 설명
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top) // 화면의 맨 위쪽 엣지까지 맵 컨텐츠를 확장하는 것을 허가
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    //.foregroundColor(.green)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
             .padding()
+            
+            Spacer()
+        }
     }
 }
 
+// View에 대한 미리보기를 선언
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by 김우성 on 2021/10/30.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    // @State 속성을 사용하면 두개 이상의 View에서 수정을 할 수 있는 앱의 데이터에 대한 source of truth를 설정할 수 있다. SwiftUI는 기본 스토리지를 관리하고 값에 따라 View를 자동으로 업데이트한다.
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.116_868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
+    var body: some View {
+        Map(coordinateRegion: $region) // 상태 변수에 $를 붙이면, 기본 값에 대한 참조와 같은 바인딩을 전달한다. 사용자가 지도와 상호 작용할 때, 지도는 현재 사용자 인터페이스에 보이는 지도의 일부와 일치하도록 지역 값을 업데이트한다.
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
